db3f01dc4d6c9f0c26bd7054d71483e0d5d88134
v2past <mycloudou@gmail.com>
2023-03-08 10:35:34 +0800

    Update Safe.sh

--- Safe.sh
+++ Safe.sh
@@ -2,6 +2,7 @@ Links:
 https://github.com/orgs/mandiant/repositories?type=all
 https://github.com/taielab
 https://blog.taielab.com/2018-10-19/network-infiltration-command.html
+https://github.com/h3110w0r1d-y/BurpLoaderKeygen
 
 
 
