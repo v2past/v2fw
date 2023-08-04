549e3e8e7f37bbe089c35d2e6d93eeb08fc4a407
v2past <mycloudou@gmail.com>
2023-08-04 10:41:51 +0800

    Create 1.Mysql.sh

---
+++ 1.mysql.sh
@@ -0,0 +1,3 @@
+https://dhc.pythonanywhere.com/article/public/1/#h4-u6570u636Eu5E93u64CDu4F5C
+
+https://www.bilibili.com/video/BV1Kr4y1i7ru/?p=18&spm_id_from=pageDriver&vd_source=24dd5fb1f74ddad3b3d11b40950314fb
